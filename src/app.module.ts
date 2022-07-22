7457e82f65e1c3a07c01a5e5b1e83e7b87bf900d
Gyujae <wjdrbwo1206@naver.com>
2022-07-22 20:45:28 +0900

    first commit :

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { PrismaModule } from './prisma/prisma.module';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    HttpModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: process.env.NODE_ENV === 'development',
+      autoSchemaFile: true,
+      playground: false,
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    UsersModule,
+    PrismaModule,
+    CoreModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
